c32e805a86fdef2f16cc5b02091bd80d98c611e9
Weixin Zeng <dexterdervish@foxmail.com>
2023-05-22 21:52:36 +0800

    Update data.py

--- src/entmatcher/extras/data.py
+++ src/entmatcher/extras/data.py
@@ -6,7 +6,7 @@ def loadfile(fn, num=1):
     ret = []
     with open(fn, encoding='utf-8') as f:
         for line in f:
-            th = line[:-1].split('\t')
+            th = line.split('\t')
             x = []
             for i in range(num):
                 x.append(int(th[i]))
